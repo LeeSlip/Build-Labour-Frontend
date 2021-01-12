4d17cac7df5a287b73053d73b29e6bb27a6278b7
LeeSlip <leonslijepcevic1@gmail.com>
2021-01-13 04:46:56 +1100

    header links, slider adjustments

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ class Header extends Component {
       <>
         <section className="header">
           <div className="header__inner">
-            <a href="/sample-page"><img src={Logo} alt="Build Labour" /></a>
+            <a href="/"><img src={Logo} alt="Build Labour" /></a>
             <nav>
               <a href="/find-work">Find Work</a>
               <a href="/find-workers">Find Workers</a>
