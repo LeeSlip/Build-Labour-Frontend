57df7b3d4371f53420a08b2e857dd033c598e7f5
LeeSlip <leonslijepcevic1@gmail.com>
2021-01-18 13:04:48 +1100

    form update

--- src/components/course-enquiry.js
+++ src/components/course-enquiry.js
@@ -1,37 +1,57 @@
 import React, { Component } from 'react'
-import addToMailchimp from 'gatsby-plugin-mailchimp'
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 class CourseEnquiry extends Component {
 
   state = {
-    name: '',
-    email: '',
-    course: '',
+    form: {
+      name: '',
+      email: '',
+      education: '',
+      qualification: '',
+      time: '',
+      motivation: '',
+      state: '',
+      suburb: '',
+    }
   }
 
-  _handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value })
+  handleChange = e => {
+    let { form } = this.state
+    form[e.target.name] = e.target.value
+    this.setState({ form })
   }
 
-  // 2. via `async/await`
-  _handleSubmit = async (e) => {
-    e.preventDefault();
-    let data = {}
-    const result = await addToMailchimp(this.state.email, this.state.name, this.state.course, data)
-    // I recommend setting `result` to React state
-    // but you can do whatever you want
-    this.setState({
-      result: result.result,
-      msg: result.msg
+  handleSubmit = e => {
+    e.preventDefault()
+    let form = document.querySelector(".contact");
+
+    if (!form.checkValidity()) return false
+
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state.form
+      })
     })
+      .then(() => this.setState({ result: 'success' }))
+      .catch(error => this.setState({ result: 'fail', msg: error }));
   }
 
   render() {
-    let props = {
-      ref: 'form',
-      name: 'newsletter',
-      className: 'subscribe-form',
-      onSubmit: this._handleSubmit,
+    let formProps = {
+      name: 'submission',
+      className: 'contact',
+      onSubmit: this.handleSubmit,
+      'data-netlify': 'true',
+      'data-netlify-honeypot': 'bot-field',
     }
 
     return (
@@ -40,13 +60,13 @@ class CourseEnquiry extends Component {
           <div className="course-enquiry__inner">
             <p className='title'>Find out more by registering your interest and details below</p>
             <a id="top" className="btn" href="/education-courses">View Courses</a>
-            <form {...props}>
+            <form {...formProps}>
               <label>What is your name?</label>
-              <input type='name' name='name' placeholder='Name' onChange={this._handleChange} required />
+              <input className='form-name' type='name' name='name' placeholder='Name' onChange={this._handleChange} required />
               <label>What is your email?</label>
-              <input type='email' name='email' placeholder='Email' onChange={this._handleChange} required />
+              <input className='form-name' type='email' name='email' placeholder='Email' onChange={this._handleChange} required />
               <label>What is your current level of education?</label>
-              <select id="education" name="education">
+              <select className='form-name' id="education" name="education">
                 <option value="Finished Year 10">Finished year 10</option>
                 <option value="Finished Year 11">Finished year 11</option>
                 <option value="Finished Year 12">Finished year 12</option>
@@ -58,8 +78,8 @@ class CourseEnquiry extends Component {
                 <option value="Finished a Bachelor">Finished a Bachelor</option>
                 <option value="Finished a Post Grad Qualification">Finished a Post Grad Qualification</option>
               </select>
-              <label>Which qualification are you interested in?</label>
-              <select type='qualification' name='qualification' onChange={this._handleChange} required>
+              <label >Which qualification are you interested in?</label>
+              <select className='form-name' type='qualification' name='qualification' onChange={this._handleChange} required>
                 <option value="Certificate III in Carpentry (CPC30211)">Certificate III in Carpentry (CPC30211)</option>
                 <option value="Certificate III in Civil Contruction Plant Operations">Certificate III in Civil Contruction Plant Operations</option>
                 <option value="Certificate IV in Building and Construction (Building) (CPC40110)">Certificate IV in Building and Construction (Building) (CPC40110)</option>
@@ -67,7 +87,7 @@ class CourseEnquiry extends Component {
                 <option value="Diploma of Building and Construction (Building) (CPC50210)">Diploma of Building and Construction (Building) (CPC50210)</option>
               </select>
               <label>When are you looking to study?</label>
-              <select type='time' name='time' onChange={this._handleChange} required>
+              <select className='form-name' type='time' name='time' onChange={this._handleChange} required>
                 <option value="ASAP">As soon as possible</option>
                 <option value="Next 3 months">Next 3 months</option>
                 <option value="Next 6 months">Next 6 months</option>
@@ -75,11 +95,12 @@ class CourseEnquiry extends Component {
                 <option value="A year or more away">A year or more away</option>
               </select>
               <label>What is your motivation to study?</label>
-              <textarea rows="4" type='motivation' name='motivation' placeholder='Motivation to study' onChange={this._handleChange} required />
+              <textarea className='form-name' rows="4" type='motivation' name='motivation' placeholder='Motivation to study' onChange={this._handleChange} required />
               <label>What state do you live in?</label>
-              <input type='state' name='state' placeholder='State' onChange={this._handleChange} required />
+              <input className='form-name' type='state' name='state' placeholder='State' onChange={this._handleChange} required />
               <label>What suburb do you live in?</label>
-              <input type='suburb' name='suburb' placeholder='Suburb' onChange={this._handleChange} required />
+              <input className='form-name' type='suburb' name='suburb' placeholder='Suburb' onChange={this._handleChange} required />
+              <input type="hidden" name="form-name" value="contact-form" />
               <button className="btn" type='submit'><p>Submit</p></button>
             </form>
           </div>
