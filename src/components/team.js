a45257cebac20253562775e4458adf8e54c61c1d
LeeSlip <leonslijepcevic1@gmail.com>
2021-03-29 22:44:32 +1100

    team start

---
+++ src/components/team.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Team extends Component {
+
+  render() {
+
+    return (
+      <>
+        <section className="team">
+
+        </section>
+      </>
+    )
+  }
+}
+
+export default Team 
